f719cdc4c9f5256ce60197eb4086f92e1fcdcd4d
BDG-arch47 <jobfraser0@gmail.com>
2025-09-29 14:11:41 -0400

    Delete practice_project directory

--- practice_project/logout.js
+++
@@ -1,5 +0,0 @@
-const logoutbtn = document.getElementById("logout");
-logoutbtn.addEventListener("click", () => { 
-    sessionStorage.removeItem("loginuser");
-    window.location.href = "index.html";
-});
